77c065e5ae7d20e2dad7d97c3179c4af15f4a2e4
Letao <letaowang7@gmail.com>
2023-02-16 23:37:12 +0100

    Add naive_algo method: 1. Add naive_algo() in main.py 2. Create run_state.py, as a class to store information of state

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
-
 # pattern matching and regexp
 import re
+from run_state import RunState
 
 f_instance = './instances/'
 
+
 class KServerInstance:
 
-  # Grid dimensions
-  # width  = 100
-  # height = 100
-  
-  def __init__(self):
-    self.k   = None  # number of technicians
-    self.opt = None  # opt value
+    # Grid dimensions
+    # width  = 100
+    # height = 100
 
-    # list of tuple coordinates. The indice i can be interpreted as a customer
-    self.sites = None   
+    def __init__(self):
+        self.k = None  # number of technicians
+        self.opt = None  # opt value
 
-    # The list of requests from the customers. 
-    # The j-th request is the one at coordinates sites[request[j]]
-    self.requests = None
+        # list of tuple coordinates. The indice i can be interpreted as a customer
+        self.sites = None
 
-  def parse(self, f_name: str) -> None:
-    rm_stuff = (lambda stuff, word: word.replace(stuff, ""))
-    to_pairs = (lambda words: list([(w.split(" ")[0], w.split(" ")[1]) for w in words]))
-    field = "# [A-Z, a-z]*\n" # pattern to split quickly over fields
-    with open(f_instance + f_name) as f:
-      data = f.read()
-      lines = re.split(field, data)
-      self.opt = int(rm_stuff("\n", lines[1]))
-      self.k = int(rm_stuff("\n", lines[2]))
-      coords_str = to_pairs(rm_stuff("\n\n", lines[3]).split("\n"))
-      self.sites = [(int(x), int(y)) for x, y in coords_str]
-      requests_str = rm_stuff("\n", lines[4]).split(" ")[:-1]
-      self.requests = list([int(w) for w in requests_str])
+        # The list of requests from the customers.
+        # The j-th request is the one at coordinates sites[request[j]]
+        self.requests = None
 
-      print(self.opt)
-      print(self.k)
-      print(self.sites)
-      print(self.requests)
+    def parse(self, f_name: str) -> None:
+        rm_stuff = (lambda stuff, word: word.replace(stuff, ""))
+        to_pairs = (lambda words: list([(w.split(" ")[0], w.split(" ")[1]) for w in words]))
+        field = "# [A-Z, a-z]*\n"  # pattern to split quickly over fields
+        with open(f_instance + f_name) as f:
+            data = f.read()
+            lines = re.split(field, data)
+            self.opt = int(rm_stuff("\n", lines[1]))
+            self.k = int(rm_stuff("\n", lines[2]))
+            coords_str = to_pairs(rm_stuff("\n\n", lines[3]).split("\n"))
+            self.sites = [(int(x), int(y)) for x, y in coords_str]
+            requests_str = rm_stuff("\n", lines[4]).split(" ")[:-1]
+            self.requests = list([int(w) for w in requests_str])
+
+            print(self.opt)
+            print(self.k)
+            print(self.sites)
+            print(self.requests)
+
+
+def naive_algo(k_instance: KServerInstance):
+    state = RunState(k_instance)
+
+    while state.num_request <= len(state.k_instance.requests) - 1:
+        distances = state.distances
+        min_distance = min(distances)
+        index = distances.index(min_distance)
+
+        state.naive_update(index)
+    print("The result is: " + str(state.sum_distance))
 
 
 if __name__ == "__main__":
-  kinstance = KServerInstance()
-  kinstance.parse("instance_N200_OPT221.inst")
+    kinstance = KServerInstance()
+    kinstance.parse("instance_N200_OPT221.inst")
+    naive_algo(kinstance)
---
+++ run_state.py
@@ -0,0 +1,75 @@
+class Server:
+    def __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+
+    def __repr__(self):
+        return "server: x: " + str(self.pos_x) + ", y: " + str(self.pos_y)
+
+    def distance_with(self, position: tuple):
+        """
+        :param position: 2 size of tuple, e.g. (13, 36)
+        Using Manhattan distance, get distance between this server and the site position
+        """
+        return abs(self.pos_x - position[0]) + abs(self.pos_y - position[1])
+
+
+class RunState:
+    def __init__(self, k_instance):
+        """
+        param k_instance:
+        servers: array of class Server.
+        num_request: the number (position) of request (sequence), it starts from 0 to N.
+        sum_distance: sum of the distances traveled by all k servers (technicians).
+        """
+        self.k_instance = k_instance
+        self.servers = []
+        self.num_request = 0
+        self.sum_distance = 0
+
+        # initiation of k servers
+        for _ in range(self.k_instance.k):
+            self.servers.append(Server())
+
+    def get_customer_site(self):
+        """
+        :return: the current site position (location of the customer that need maintenance service),
+        return type: a tuple, e.g. (12, 36).
+        """
+        x = self.k_instance.sites[self.k_instance.requests[self.num_request]][0]
+        y = self.k_instance.sites[self.k_instance.requests[self.num_request]][1]
+        # print("num_request: " + str(self.num_request))
+        # print("(x, y): " + str(x) + "," + str(y))
+        return x, y
+
+    @property
+    def distances(self):
+        """
+        :return: k size array of distances between customer site and i-th servers
+        """
+        distances = []
+        for server in self.servers:
+            distance = server.distance_with(self.get_customer_site())
+            distances.append(distance)
+        return distances
+
+    def naive_update(self, selected_server: int):
+        """
+        :param selected_server: number of server that treat request
+        """
+        if self.num_request == len(self.k_instance.requests):
+            print("It is over!")
+        else:
+            distance = self.distances[selected_server]
+            self.sum_distance = self.sum_distance + distance
+
+            # print("num_request: " + str(self.num_request))
+            # print("selected server number: " + str(selected_server) + ", " + str(self.servers[selected_server]))
+            # print("distances: " + str(self.distances))
+
+            self.servers[selected_server].pos_x, self.servers[selected_server].pos_y = self.get_customer_site()
+
+            # print("Updated, sum distance: " + str(self.sum_distance))
+            # print("---------------------------------------")
+
+            self.num_request += 1
